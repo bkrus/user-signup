f82073ed0882b736849b1451f60754b31143ff77
Robert Krussel <bkrus17@gmail.com>
2017-09-29 22:56:38 -0500

    added blank field validations

--- main.py
+++ main.py
@@ -5,23 +5,40 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-def is_empty (input):
-    if input == '':
-        return True
-
 @app.route('/')
 def display_index():
     return render_template('index.html')
+
+def is_empty (n):
+        if n == "":
+            return True
 
 @app.route('/', methods=['post'])
 def validate_index():
     username = request.form['username']
     username_error= ''
+
+    password = request.form['password']
+    password_error= ''
+
+    verify = request.form['verify']
+    verify_error= ''
     
-    if is_empty(username) == True:
+    if is_empty (username):
         username_error = 'Username cannot be empty'
         username = ''
-        return render_template('index.html', username_error=username_error)
+        
+        #return render_template('index.html', username_error=username_error)
+
+    if is_empty(password):
+        password_error = 'Password cannot be empty'
+        password = ''
+    
+    if is_empty(verify):
+        verify_error = ' Verify Password cannot be empty'
+        verify = ''   
+        return render_template('index.html', username_error=username_error, password_error=password_error, verify_error=verify_error)
+    
     else:
         return render_template('welcome.html', username=username)
      
