ccdaffba14a879d3fa7251467c49f1907e38568c
Robert Krussel <bkrus17@gmail.com>
2017-10-02 20:16:57 -0500

    fixed validation errors

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ def is_empty (n): #Check if a string is empty
             return True
 
 def is_char(letters): #Check if letters in a string do not contain spaces or special characters
-    for i in letters.lower:
-        if i in string.ascii_lowercase:
-            return True
+    for i in letters:
+        if not i.isalpha():
+            return False
+    return True
 
 @app.route('/', methods=['post'])
 def validate_index():
@@ -34,22 +35,23 @@ def validate_index():
     verify_error= ''
     
     #Username validations
-    if is_empty (username): 
+    if is_empty(username): 
         username_error = 'Username cannot be empty'
-        username = ''
+
     #Password validations
     if is_empty(password): 
             password_error = 'Password cannot be empty'
             password = ''
     
-    if len(password) < 3 and len(password) >20:
+    if len(password) < 3 or len(password) >20:
         password_error = 'Password must be between 3 and 20 characters'
-        password = ''
+        #password = ''
     
-    else: 
-        if not is_char(password):
-            password_error = 'Password can only contain letters'
-            password = ''
+    
+    if not is_char(password):
+        password_error = 'Password can only contain letters'
+        
+        #password = ''
 
     #Verify Password Validations   
     if is_empty(verify):
@@ -59,7 +61,8 @@ def validate_index():
     if verify != password:
         verify_error = ' Verify Password must match Password'
         verify = '' 
-            
+
+    if username_error or password_error or verify_error:
         return render_template('index.html', username_error=username_error, password_error=password_error, verify_error=verify_error)
     
     else:
