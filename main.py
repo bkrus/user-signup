50a6aa8ba56e5a18d745cd0022cd54e35b12b868
Robert Krussel <bkrus17@gmail.com>
2017-10-03 19:17:44 -0500

    email validations and completed project

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import cgi
 import os
 import string
 
-
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -33,6 +31,9 @@ def validate_index():
 
     verify = request.form['verify']
     verify_error= ''
+
+    email = request.form['email']
+    email_error= ''
     
     #Username validations
     if is_empty(username): 
@@ -41,17 +42,14 @@ def validate_index():
     #Password validations
     if is_empty(password): 
             password_error = 'Password cannot be empty'
-            password = ''
     
     if len(password) < 3 or len(password) >20:
         password_error = 'Password must be between 3 and 20 characters'
-        #password = ''
-    
+        password = ''
     
     if not is_char(password):
         password_error = 'Password can only contain letters'
-        
-        #password = ''
+        password = ''
 
     #Verify Password Validations   
     if is_empty(verify):
@@ -61,10 +59,39 @@ def validate_index():
     if verify != password:
         verify_error = ' Verify Password must match Password'
         verify = '' 
-
-    if username_error or password_error or verify_error:
-        return render_template('index.html', username_error=username_error, password_error=password_error, verify_error=verify_error)
     
+    #email validatations
+    if not is_empty(email): 
+        if '@' not in email:
+            email_error = 'Email is not valid'
+            email = ''
+        
+        if '.' not in email:
+            email_error = 'Email is not valid'
+            email = ''
+        
+        if len(email) < 3 or len(email) >20:
+            email_error = 'Email is not valid'
+            email = ''
+        
+        if email.count('@') > 1:
+            email_error = 'Email is not valid'
+            email = ''
+        
+        if email.count('.') > 1:
+            email_error = 'Email is not valid'
+            email = ''
+    
+    #render screen with error messages
+    if username_error or password_error or verify_error or email_error:
+        return render_template('index.html', 
+                                username=username, 
+                                email=email, 
+                                username_error=username_error, 
+                                password_error=password_error, 
+                                verify_error=verify_error, 
+                                email_error=email_error)
+    #render Welcome 
     else:
         return render_template('welcome.html', username=username)
      
